13096ed709536d82ecdfada86ee28fbfd24daa34
Aditek <aditek1105@gmail.com>
2020-12-24 13:56:01 -0500

    React Advanced

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ErrorEg from "./tutorial/1-useState/setup/1-error-example";
+function App() {
+  return (
+    <div className="container">
+      <ErrorEg />
+    </div>
+  );
+}
+
+export default App;
